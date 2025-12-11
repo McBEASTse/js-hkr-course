3e57dd98cc826fe56f4ef9af071b4d532d69184a
Marcus Pettersson <marcus.pettersson@dtms.se>
2025-12-11 16:22:32 +0100

    reworking css and classes, no more tailwind light

--- lab1/scripts/menu-button.js
+++
@@ -1,9 +0,0 @@
-const btn = document.getElementById("menu-button");
-const menu = document.querySelector('nav');
-
-btn.addEventListener('click', toggle);
-
-function toggle() {
-    menu.classList.toggle('active');
-    btn.classList.toggle('active');
-}
